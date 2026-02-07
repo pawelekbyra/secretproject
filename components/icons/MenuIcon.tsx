f5e6edd0a5dda4b8cc64b1a476e81a723d792504
pawelekbyra <pawelek.byra@gmail.com>
2026-02-07 20:14:10 +0100

    Update MenuIcon.tsx

--- components/icons/MenuIcon.tsx
+++ components/icons/MenuIcon.tsx
@@ -1,18 +1,22 @@
-import * as React from "react";
+import React from 'react';
 
-const MenuIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth={2.0}
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-  >
-    <path d="M3 12h18M3 6h18M3 18h18" />
-  </svg>
-);
-
-export default MenuIcon;
+export default function MenuIcon({ className }: { className?: string }) {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="24" 
+      height="24" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke="currentColor" 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
+      className={className}
+    >
+      <line x1="3" y1="12" x2="21" y2="12"></line>
+      <line x1="3" y1="6" x2="21" y2="6"></line>
+      <line x1="3" y1="18" x2="21" y2="18"></line>
+    </svg>
+  );
+}
